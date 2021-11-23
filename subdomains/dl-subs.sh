108047748d21d467bfaa55ed5962927d33e9c187
Felipe Caon <felipecaon@gmail.com>
2021-11-23 11:29:58 -0300

    more

--- subdomains/dl-subs.sh
+++ subdomains/dl-subs.sh
@@ -1,3 +1,6 @@
 wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/dns-Jhaddix.txt -O jhaddix.txt
 wget https://wordlists-cdn.assetnote.io/data/manual/2m-subdomains.txt -O 2msubs.txt
 wget https://wordlists-cdn.assetnote.io/data/manual/best-dns-wordlist.txt -O best-dns-wl.txt
+wget https://raw.githubusercontent.com/bitquark/dnspop/master/results/bitquark_20160227_subdomains_popular_100000 -O bitquark.txt
+
+cat *.txt >> subdomains
